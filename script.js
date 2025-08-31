00985c31a80f7a158174ef7cc2ea332ec4813edb
Jee <182109647+Aicyraa@users.noreply.github.com>
2025-08-31 14:37:54 +0800

    Fix js issue

--- script.js
+++ script.js
@@ -12,22 +12,23 @@ toggleCmd.addEventListener("click", (e) => {
 
 const tablinks = document.querySelectorAll(".tablinks");
 
-tablinks.forEach(element => {
-    element.addEventListener("click", (e) => {
-      let i, tabcontent, tablinks;
-      let datalink = e.currentTarget.dataset.tab
-      console.log(datalink);
-      
-      tabcontent = document.getElementsByClassName("tab");
-      tablinks = document.getElementsByClassName("tablinks");
-    
-      for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-        tablinks[i].classList.remove('active')
-      }
-    
-      document.getElementById(datalink).style.display = "block";
-      e.currentTarget.classList.add('active')
-    });
-})
+tablinks.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    let i, tabcontent, tablinks;
+    let datalink = e.currentTarget.dataset.tab;
+    console.log(datalink);
 
+    tabcontent = document.getElementsByClassName("tab");
+    tablinks = document.getElementsByClassName("tablinks");
+
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+      tablinks[i].classList.remove("active");
+    }
+
+    if (datalink !== undefined) {
+      document.getElementById(datalink).style.display = "block";
+      e.currentTarget.classList.add("active");
+    }
+  });
+});
